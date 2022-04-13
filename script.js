10bb7cd21927c2cf6920600edbcaf8320f71031c
Hawkz2119 <diasakhmetov2119@gmail.com>
2022-04-14 00:27:49 +0200

    switch

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-var name = prompt("What is your name?")
+// var name = prompt("What is your name?")
 
-var q1 = prompt(name + "," + "what is the capital of Estonia? 1. Helsinki 2. Tallin 3. Riga 4. Vilnus")
-    if (q1 ==2) {
-        alert("Good job, my friend")
-    }
-    else {
-        alert ("U fuckt up, bastard")
-    }
-    var q2 = prompt(name + "," + "what is the capital of Moldovian Republic? 1. Chisinau 2. Bratislava 3. Bucharest 4. Belgrad")
-    if (q2 ==1) {
-        alert("You are awesome, bro")
-    }
-    else{
-        alert ("Get the fuck out of here, fool")
-    }
-    var q3 = prompt(name + "," + "what is the capital of Armenia? 1. Tbilisi 2. Erevan 3. Batumi 4. Baku")
-    if (q3 ==2) {
-        alert("Nicely done, genius")
-    }
-    else{
-        alert ("Get wrecked, moron")
-    }
-    var q4 = prompt(name + "," + "what is the capital of Australia? 1. Melbourn 2. Canberra 3. Sydney 4. Kardiff")
-    if (q4 ==2) {
-        alert("Thats all, mr. Einstein")
-    }
-    else{
-        alert ("You have to graduate from ur fucking school, jerk")
-    }
+// var q1 = prompt(name + "," + "what is the capital of Estonia? 1. Helsinki 2. Tallin 3. Riga 4. Vilnus")
+//     if (q1 ==2) {
+//         alert("Good job, my friend")
+//     }
+//     else {
+//         alert ("U fuckt up, bastard")
+//     }
+//     var q2 = prompt(name + "," + "what is the capital of Moldovian Republic? 1. Chisinau 2. Bratislava 3. Bucharest 4. Belgrad")
+//     if (q2 ==1) {
+//         alert("You are awesome, bro")
+//     }
+//     else{
+//         alert ("Get the fuck out of here, fool")
+//     }
+//     var q3 = prompt(name + "," + "what is the capital of Armenia? 1. Tbilisi 2. Erevan 3. Batumi 4. Baku")
+//     if (q3 ==2) {
+//         alert("Nicely done, genius")
+//     }
+//     else{
+//         alert ("Get wrecked, moron")
+//     }
+//     var q4 = prompt(name + "," + "what is the capital of Australia? 1. Melbourn 2. Canberra 3. Sydney 4. Kardiff")
+//     if (q4 ==2) {
+//         alert("Thats all, mr. Einstein")
+//     }
+//     else{
+//         alert ("You have to graduate from ur fucking school, jerk")
+//     }
 
+
+    let num = prompt('2 + 2')
+
+    switch(num){
+      case '1':
+        console.log('false')
+        break
+      case '2':
+        console.log('false')
+        break
+      case '3':
+        console.log('false')
+        break
+      case '4':
+        console.log('true')
+        break
+        
+        default:
+        console.log('nothing')
+              
+    }
